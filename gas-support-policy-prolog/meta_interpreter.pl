e7b92a6388904480c3e4a939ac743d7936370168
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2023-09-15 23:30:00 +0200

    Tree representation ready

--- gas-support-policy-prolog/meta_interpreter.pl
+++ gas-support-policy-prolog/meta_interpreter.pl
@@ -1,73 +1,62 @@
-% mi(true,_, _).
-% mi((A, B), N, Trace) :-
-%     mi(A, N, TraceA),
-%     length(TraceA, L),
-%     M is N + L, 
-%     mi(B, M, TraceB),
-%     append(TraceA, TraceB, Trace).
-% mi(Goal, N,[(N, Goal :- Body, Unification)|Trace]) :-
-%     Goal \= true,
-%     Goal \= (_,_),
-%     clause(Goal, Body),
-%     NewGoal = Goal,
-%     copy_term((Goal, Body), (NewGoal, NewBody)),
-%     unification_trace(Body, NewBody, Unification),
-%     M is N + 1,
-%     mi(Body, M, Trace).
-
-
+:- include("./policy.pl").
+:- include("./testprograms.pl").
 
 % Proof tree
-% ?- mi_proof_tree(ancestor(anne,carol),T). 
-% T = ((true=>parent(anne, bob), (true=>parent(bob, carol))=>ancestor(bob, carol))=>ancestor(anne, carol)) 
-:- op(750, xfy, =>).
+:- op(750, xfy, :>).
 
-mi_proof_tree(true, true).
-mi_proof_tree((A, B), (TreeA, TreeB)) :-
+% end(leaf) of the proof tree
+mi_proof_tree(true, [true]).
+% conjunction
+mi_proof_tree((A, B), Tree) :-
     !,
     mi_proof_tree(A, TreeA),
-    mi_proof_tree(B, TreeB).
-
+    mi_proof_tree(B, TreeB),
+    append([TreeA, TreeB], Tree).
+% disjunciton
+mi_proof_tree((A; _), Tree) :-
+    mi_proof_tree(A, Tree).
+mi_proof_tree((_; B), Tree) :-
+    mi_proof_tree(B, Tree).
 % handle built-in predicates
 mi_proof_tree(A, Trace) :-
     predicate_property(A, built_in),
-    Trace = (true => A),
+    Trace = [[true] :> A],
     !,
     call(A).
-mi_proof_tree(G, Tree => G) :-
+% general case
+mi_proof_tree(G, [Tree :> G]) :-
     G \= true,
     G \= (_,_),
     G \= (_\=_),
     clause(G, B),
     mi_proof_tree(B, Tree).
 
-
-unification_trace(A, B, []) :- var(A), var(B), !.
-unification_trace((A, C), (B, D), [(A, B), (C, D)]) :- !.
-unification_trace(A, B, [(A, B)]).
-
-
-
-
-
-
-
-
-
-prove(A) :-
-    mi_proof_tree(A,Trace),
-    print_trace(Trace).
-
-% print_trace([]).
-% print_trace([(N, A :- B, Substitution)|Trace]) :-
-%     write("Step:"), write(N),nl,
-%     write(A), write(" :- "), write(B), write(" "), write(Substitution), nl,
-%     print_trace(Trace).
-
-print_trace(Trace):- 
+print_proof_tree(A):- 
+    mi_proof_tree(A, Trace),
     write("Proof tree: "), write(Trace), nl.
 
 
+prove(A):-
+    mi_proof_tree(A,Trace),
+    print_tree(Trace).
 
-:- include("./policy.pl").
-:- include("./testprograms.pl").
+
+% 
+print_tree(Tree):-
+    print_tree(Tree, 0).
+print_tree([true], Indent):-
+    tab(Indent), 
+    write(true), nl.
+print_tree([Items:>Goal], Indent):- 
+    tab(Indent), 
+    write(Goal), nl,
+    NewIndent is Indent + 4,
+    print_children(Items, NewIndent).
+    % print_tree(NextItem, NewIndent).
+
+print_children([], _).
+print_children([Item|Items], Indent):- 
+    print_tree([Item], Indent),
+    print_children(Items, Indent).
+
+
