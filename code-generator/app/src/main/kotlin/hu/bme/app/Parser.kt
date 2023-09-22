9d62dea90a9941daee2e1d5f1d1cca82fc18819d
Balazs Toldi <balazs@toldi.eu>
2023-09-22 10:45:20 +0200

    Better parsing options

--- code-generator/app/src/main/kotlin/hu/bme/app/Parser.kt
+++ code-generator/app/src/main/kotlin/hu/bme/app/Parser.kt
@@ -4,15 +4,38 @@ object Parser {
     fun parseProlog(prologCode: String): List<Clause> {
         val clauses = mutableListOf<Clause>()
 
-        prologCode.lines().forEach { line ->
-            val trimmedLine = line.trim()
-            if (trimmedLine.isNotEmpty()) {
-                clauses.add(parseClause(trimmedLine))
-            }
+        val mergedLines = preprocessPrologCode(prologCode)
+        for (line in mergedLines) {
+            clauses.add(parseClause(line))
         }
 
         return clauses
     }
+
+
+    fun preprocessPrologCode(prologCode: String): List<String> {
+        val lines = prologCode.lines()
+        val mergedLines = mutableListOf<String>()
+        var currentLine = StringBuilder()
+
+        for (line in lines) {
+            val trimmedLine = line.trim()
+            if (trimmedLine.isNotEmpty()) {
+                currentLine.append(trimmedLine).append(" ")
+                if (trimmedLine.endsWith('.')) {
+                    mergedLines.add(currentLine.toString())
+                    currentLine = StringBuilder()
+                }
+            }
+        }
+
+        if (currentLine.isNotEmpty()) {
+            mergedLines.add(currentLine.toString())
+        }
+
+        return mergedLines
+    }
+
 
     fun parseClause(clauseStr: String): Clause {
         val parts = clauseStr.split(":-")
@@ -31,11 +54,14 @@ object Parser {
         val predicates = mutableListOf<String>()
         var start = 0
         var depth = 0
+        var listDepth = 0  // to handle lists
 
         for ((index, char) in bodyStr.withIndex()) {
             if (char == '(') depth++
             if (char == ')') depth--
-            if (char == ',' && depth == 0) {
+            if (char == '[') listDepth++
+            if (char == ']') listDepth--
+            if (char == ',' && depth == 0 && listDepth == 0) {
                 predicates.add(bodyStr.substring(start, index).trim())
                 start = index + 1
             }
@@ -48,7 +74,7 @@ object Parser {
 
     fun parsePredicate(predStr: String): Predicate {
         val name = predStr.substringBefore("(").trim()
-        val termsStr = if (predStr.contains("(")) predStr.substringAfter("(").trimEnd(')', '.').trim() else ""
+        val termsStr = if (predStr.contains("(")) predStr.substringAfter("(").trimEnd(')', '.',',').trim() else ""
         val terms = if (termsStr.isNotEmpty()) {
             termsStr.split(",").map { parseTerm(it.trim()) }
         } else {
@@ -61,7 +87,13 @@ object Parser {
 
 
     fun parseTerm(termStr: String): Term {
-        val trimmedTerm = termStr.trimEnd('.')
+        val trimmedTerm = termStr.trimEnd('.').trim()
+
+        // Handle list structures, assuming simple cases for now
+        if (trimmedTerm.startsWith("[") && trimmedTerm.endsWith("]")) {
+            return Atom(trimmedTerm)
+        }
+
         return when {
             trimmedTerm.first().isUpperCase() -> Variable(trimmedTerm)
             else -> Atom(trimmedTerm)
