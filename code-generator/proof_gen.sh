5dcac26b84685bcb2b2aeb01b464c0dc67d16647
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2023-11-02 00:29:38 +0100

    ran proof

--- code-generator/proof_gen.sh
+++ code-generator/proof_gen.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # This script generates the proof for the circuit in the file
-if  ! test -f "powersOfTau28_hez_final_16.ptau"; then
-    echo "Downloading powersOfTau28_hez_final_10.ptau"
-    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final.ptau
+if  ! test -f "powersOfTau28_hez_final_22.ptau"; then
+    echo "Downloading powersOfTau28_hez_final_22.ptau"
+    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_22.ptau
 fi
-
-snarkjs plonk setup generated.r1cs powersOfTau28_hez_final_21.ptau circuit_final.zkey
+startPlonkSetup=$(date +%s)
+echo "Plonk setup start time: $startPlonkSetup"
+node --max-old-space-size=51200 /usr/local/bin/snarkjs plonk setup policy.r1cs powersOfTau28_hez_final_22.ptau circuit_final.zkey
+endPlonkSetup=$(date +%s)
+echo "Plonk setup time: $((endPlonkSetup-startPlonkSetup))s"
 
 time snarkjs plonk prove circuit_final.zkey witness.wts proof.json public.json
