81268c3fcd67cf651afce58802a368166267dd8d
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2024-11-24 20:58:21 +0100

    Upd deps and gradle

--- code-generator/common/build.gradle.kts
+++ code-generator/common/build.gradle.kts
@@ -3,12 +3,11 @@ plugins {
     antlr
 }
 dependencies {
-    implementation("org.json:json:20230227")
-    implementation("org.antlr:antlr4:4.13.1")
+    implementation("org.json:json:20231013")
+    implementation("org.antlr:antlr4:4.13.2")
     // https://mvnrepository.com/artifact/com.owlike/genson
-    implementation("com.owlike:genson:1.6")
-
-    antlr("org.antlr:antlr4:4.13.1")
+//    implementation("com.owlike:genson:1.6")
+    antlr("org.antlr:antlr4:4.13.2")
 }
 
 tasks.named("compileKotlin") {
